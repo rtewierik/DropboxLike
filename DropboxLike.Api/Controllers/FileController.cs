4a44fa8511111b3fd83269ce5bf8a8e96aa9576d
Godfrey Owidi <godfreyowiidi@gmail.com>
2023-05-24 21:45:25 +0300

    Add download to dir & error handling for download example in controllers.

--- DropboxLike.Api/Controllers/FileController.cs
+++ DropboxLike.Api/Controllers/FileController.cs
@@ -1,5 +1,9 @@
+using DropboxLike.Domain.Models;
+using DropboxLike.Domain.Repositories;
 using DropboxLike.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
+using File = DropboxLike.Domain.Models.File;
+
 
 namespace DropboxLike.Api.Controllers;
 
@@ -22,28 +26,30 @@ public class FileController : ControllerBase
     
     // TODO: Find a way to return result with correct status code (in case of failure 404, 500, et cetera) using output of FileService call).
 
-    return Ok(response);
+    return Ok(response.Successful);
   }
 
   [HttpGet]
   [Route("Download/{fileId}")]
   public async Task<IActionResult> DownloadFileAsync(string fileId)
   {
-    // var destinationFolderPath = $"/home/godfreyowidi/Downloads/TestsDowloads";
-
-    var result = await _fileService.DownloadSingleFileAsync(fileId);
-    if (result.Successful)
+    var response = await _fileService.DownloadSingleFileAsync(fileId);
+    try
     {
-      var file = result.Result!;
-      // TODO: Consume this result when returning some response.
-      var fileStreamResult = new FileStreamResult(file.FileStream, file.ContentType);
-      return Ok();
+      if (response.Successful)
+      {
+        var file = response.Result;
+        return new FileStreamResult(file.FileStream, file.ContentType);
+      }
+      else
+      {
+        return BadRequest($"Failed to read, {fileId}: {response.FailureMessage!}");
+      }
     }
-    // TODO: Here we have to return some failure response indicating our download operation failed.
-    var exception = result.Exception; // TODO: Be careful! Potentially null!
-    var message = result.FailureMessage!;
-    // If message is "500: Some internal error occurred", we want to return response with status code 500 and response body "Some internal error occurred".
-    return Ok();
+    catch (Exception ex)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, $"Failed to read, {fileId}: {ex.Data}-{response.Exception}");
+    }
   }
 
   [HttpDelete]
